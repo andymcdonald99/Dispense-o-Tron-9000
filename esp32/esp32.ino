8073194e916b23d9378b95cbc538beefc308e697
GH-James <78970503+GH-James@users.noreply.github.com>
2023-02-14 16:43:24 +0000

    API access to database working

--- esp32/esp32.ino
+++ esp32/esp32.ino
@@ -1,4 +1,6 @@
 #include <WiFi.h>
+#include <HTTPClient.h>
+
 
 const char* ssid = "insert ssid";
 const char* password = "insert password";
@@ -7,7 +9,13 @@ void setup(){
     Serial.begin(115200);
     delay(1000);
 
-    WiFi.mode(WIFI_STA); //Optional
+    String serverName = "https://dispens-o-tron-default-rtdb.europe-west1.firebasedatabase.app";
+    String path = "/users.json";
+    String args = "?auth=insert api key";
+
+    Serial.println(ssid);
+    Serial.println(password);
+    WiFi.mode(WIFI_STA);
     WiFi.begin(ssid, password);
     Serial.println("\nConnecting");
 
@@ -19,6 +27,31 @@ void setup(){
     Serial.println("\nConnected to the WiFi network");
     Serial.print("Local ESP32 IP: ");
     Serial.println(WiFi.localIP());
+
+    HTTPClient http;
+    WiFiClientSecure client;
+    client.setInsecure();
+
+    String serverPath = serverName + path + args;
+    
+    http.begin(client, serverPath);
+    
+    int httpResponseCode = http.GET();
+    Serial.println("response code");
+    Serial.println(httpResponseCode);
+
+    if (httpResponseCode > 0) {
+      Serial.println("HTTP Response code: ");
+      Serial.println(httpResponseCode);
+      String payload = http.getString();
+      Serial.println(payload);
+    }
+    else {
+      Serial.println("Error code: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+
 }
 
 void loop(){}
